05c0db84e6f1d346244c6d807a75cf4d26a4c332
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-05-07 17:10:41 -0300

    Header terminado

--- Barbearia_baianinho/src/componentes/Header.jsx
+++ Barbearia_baianinho/src/componentes/Header.jsx
@@ -1,24 +1,33 @@
+import Sytles from './Header.module.css'
 import logo1 from '../imagens/logoheader.png'
 import baiano from '../imagens/Bahianinho.png'
 function Header(){
     return(
     <div>
         <header>
-            <div className='logo'>
-                <img src={logo1} alt="" />
-                <img src={baiano} alt="" />
+            <div className={Sytles.logo}>
+                <img src={logo1} alt=""  className={Sytles.logo_header}/>
+                <img src={baiano} alt="" className={Sytles.nome}/>
             </div>
 
             <nav>
                 <li>
-                    <ul>Quem somos</ul>
-                    <ul>Contato</ul>
-                    <ul>Serviços</ul>
-                    <ul>Localização</ul>
+                    <a href="" className={Sytles.link_nav}>
+                        <ul>Quem somos</ul>
+                    </a>
+                    <a href="" className={Sytles.link_nav}>
+                        <ul>Contato</ul>
+                    </a>
+                    <a href="" className={Sytles.link_nav}>
+                        <ul>Serviços</ul>
+                    </a>
+                    <a href="" className={Sytles.link_nav}>
+                        <ul>Localização</ul>
+                    </a>
                 </li>
             </nav>
-            <div className="phone">
-                <a href="">Entre em contato</a>
+            <div className={Sytles.phone}>
+                <a href="" className={Sytles.contact}>Entre em contato</a>
             </div>
         </header>
     </div>
