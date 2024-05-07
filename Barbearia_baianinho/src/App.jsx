d37cb9c9138ce3530a9fcc07ad67ab34ffd015f7
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-05-07 20:10:04 -0300

    .

--- Barbearia_baianinho/src/App.jsx
+++ Barbearia_baianinho/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from '../src/componentes/Header'
+import Banner from '../src/componentes/Banner'
 function App() {  
  
 
   return (
     <>
       <Header />
+      <Banner />
     </>
   )
 }
---
+++ Barbearia_baianinho/src/componentes/Banner.jsx
@@ -0,0 +1,24 @@
+import Styles from '../componentes/Banner.module.css'
+import Logo_banner from'../imagens/logo.png'
+import Barber from '../imagens/frente_banner.png'
+
+function Banner(){
+    return(
+        <div>
+            <div className={Styles.Image_background}>
+                <img src={Barber} alt="" />
+
+                <h1>
+                A Melhor Barbearia de Taboão da Serra
+                </h1>
+                
+            </div>
+            <div className={Styles.bx_banner}>
+                <img src={Logo_banner} alt="" />
+            </div>
+
+           
+        </div>
+    )
+}
+export default Banner
